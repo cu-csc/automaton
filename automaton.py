75dde836a5b030c7305f8f6d32443f5f4382323a
Dmitry Duplyakin <duplyakin@gmail.com>
2012-11-05 16:37:07 -0700

    Added config files: global.conf, benchmarking.conf, clouds.conf Added lib/config.py with classes that retrieve and store options from the config files Implemented classes Cloud, Clouds, Cluster, Clusters
    Update #1: changed Log.info to Log.debug
    Update #2: removed etc/automaton.conf
    Update #3: added a check before registering a key
    Update #4: replaced string concatenation in %s
    Update #5: added file default locations in the help for command line arguments
    Update #6: replaces old function names with log_info and get_fqdns

--- automaton.py
+++ automaton.py
@@ -2,26 +2,36 @@
 
 import logging
 import signal
+from threading import Thread
 
 from lib.logger import configure_logging
 from lib.util import parse_options
 from lib.util import read_config
-from threading import Thread
-
+from lib.config import Config
+from resources.cloud.clouds import Cloud, Clouds
+from resources.cluster.clusters import Clusters
 
 SIGEXIT = False
 LOG = logging.getLogger(__name__)
 
 
 class Automaton(Thread):
-    def __init__(self, config):
+    def __init__(self, config, clusters):
         Thread.__init__(self)
         self.config = config
+        self.clusters = clusters
 
     def run(self):
         LOG.info("Starting Automaton")
         #TODO(pdmars): do something
 
+        # Code below demonstrates functionality of Cluster class:
+        # for cluster in self.clusters.list:
+        #    cluster.connect()
+        #    cluster.launch()
+        #    cluster.log_info()
+        #    fqdns = cluster.get_fqdns()
+        #    cluster.terminate_all()
 
 def clean_exit(signum, frame):
     global SIGEXIT
@@ -29,13 +39,15 @@ def clean_exit(signum, frame):
     LOG.critical("Exit signal received. Exiting at the next sane time. "
                  "Please stand by.")
 
-
 def main():
     (options, args) = parse_options()
     configure_logging(options.debug)
-    config = read_config(options.config_file)
+
+    config = Config(options)
+    clusters = Clusters(config)
+
     signal.signal(signal.SIGINT, clean_exit)
-    automaton = Automaton(config)
+    automaton = Automaton(config, clusters)
     automaton.start()
     # wake every seconed to make sure signals are handled by the main thread
     # need this due to a quirk in the way Python threading handles signals
