b9cdcfb4b1e72b22d1bcab3fdabc2fe837aa1ca9
Ali Alzabarah <ali.alzabarah@cs.colorado.edu>
2012-11-07 23:39:32 -0700

    add skeleton of the staged deployment add the core functionality of the staged deployment add tests cases for the new functionality add demonstration of staged deployment add requirements.txt for pip
    # updates :
    move the engine to examples folder
    add not implemented exception for deployment/engine
    add debug to check_port_status
    convert run_remote_command from function to a class
    create new common module in deployment
    move common deployment functionality to deployment package
    documented how to run tests cases for non-PyCharm users :)
    changed the RemoteCommand to return return_code or None

---
+++ deployment/engine.py
@@ -0,0 +1,9 @@
+"""
+implements the staged deployment engine which contains the logic and
+order of execution
+"""
+
+class StagedDeploymentEngine(object):
+
+    def __init__(self):
+        raise NotImplementedError
---
+++ deployment/executor.py
@@ -0,0 +1,27 @@
+"""
+Module that handle the staged deployment execution
+"""
+
+from lib import util
+
+
+class Executor(object):
+
+    def __init__(self, hostname, private_key, staged_dict):
+        self.hostname = hostname
+        self.private_key = private_key
+        self.staged_dict = staged_dict
+
+    def execute_one_level(self, run_level):
+
+        result_dict = {}
+        cmds_in_run_level = self.staged_dict[run_level]
+
+        for command in cmds_in_run_level:
+            remote_command = util.RemoteCommand(self.hostname, self.private_key, command)
+            return_code = remote_command.execute()
+            result_dict[command] = (return_code, remote_command.stdout, remote_command.stderr)
+
+            if return_code != 0:
+                break
+        return result_dict
